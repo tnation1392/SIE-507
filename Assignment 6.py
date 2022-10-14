45064ae62fe7773c458e2727484ca9a94804f402
tnation1392 <96146191+tnation1392@users.noreply.github.com>
2022-10-14 09:05:02 -0400

    Update Assignment 6.py

--- Assignment 6.py
+++ Assignment 6.py
@@ -1,3 +1,14 @@
 #Todd Nason Assignment 6
 #Program to display random samples of fish weights from Maine lakes
 
+sample_pond = {
+    1000 : "Chemo",
+    1010 : "Greene",
+    1050 : "Hopkins",
+    1100 : "Toddy"}
+    #A dictionary containing the pond ID's and their associated names
+
+sample_fish_pond = [1000, 1010, 1050, 1000, 1100, 1010, 1050, 1010] # a list of the given sample pond ID's
+sample_fish_lb = [4.0, 2.0, 1.5, 2.0, 2.2, 1.9, 2.8, 3.5] # a list of the given sample fish weights 
+
+print(sample_fish_pond[1], sample_fish_lb[1])
